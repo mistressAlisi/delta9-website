cb816482f5f7046d58896a61705fa83eafc0346a
Rasheek Draconis <rasheekdrg@gmail.com>
2021-08-20 18:48:47 -0400

    Nav fixes for mobile / introducing mobile home

--- d9NX3/presentation/views.py
+++ d9NX3/presentation/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def home(request):
-    return render(request,"presentation-index.html",{})
+    if (request.META['HTTP_HOST'] == "m.del9.biz"):
+        return redirect('home_mobile')
+    else:
+        return render(request,"presentation-index.html",{})
 
 def home_2d(request):
     return render(request,"presentation-index-2d.html",{})
@@ -12,3 +15,7 @@ def benefits(request):
 
 def spectrum(request):
     return render(request,"presentation-spectrum.html",{})
+
+
+def home_mobile(request):
+    return render(request,"presentation-index-mobi.html",{})
